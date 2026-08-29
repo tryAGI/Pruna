eaf0e70646c83772c97762b349a8312fb552cf88
Konstantin S. <havendv@gmail.com>
2026-08-29 13:56:38 +0400

    feat: Updated OpenAPI spec (#38)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Pruna/Generated/Pruna.JsonSerializerContext.g.cs
+++ src/libs/Pruna/Generated/Pruna.JsonSerializerContext.g.cs
@@ -28,6 +28,7 @@ namespace Pruna
             typeof(global::Pruna.JsonConverters.UnixTimestampJsonConverter),
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Pruna.JsonSerializerContextTypes))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Pruna.ModelId), TypeInfoPropertyName = "ModelId2")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Pruna.CreatePredictionRequest))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Pruna.PredictionInput))]
